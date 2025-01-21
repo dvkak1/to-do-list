ec5ccedd4b46ae320cb1bc378bd4ca4f46f0e261
dvkak1 <dv_kakilala@yahoo.com>
2025-01-21 15:32:09 +0800

    create functionality added

--- app.js
+++ app.js
@@ -14,18 +14,37 @@ let posts = document.getElementById("posts");
 //from the form.
 form.addEventListener("submit", (e) => {
    e.preventDefault();
-   console.log("button clicked");
-
    formValidation();
 });
 
+
+//Contains if else statement inside formValidation function. 
 let formValidation = () => {
     if(input.value === "") {
         msg.innerHTML = "Post cannot be blank";
-        console.log("failure");
     } else {
-        console.log("success");
         msg.innerHTML = "";
+        acceptData();
     }
 };
 
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+    
+    createPost();
+};
+
+let createPost = () => {
+    posts.innerHTML += `  
+     <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>`;
+  input.value = "";
+}
